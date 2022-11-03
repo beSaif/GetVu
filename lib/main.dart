4d79139e88abf625d06096f72ca06a2d0616f4ac
jumainahkhan <jumainahkhan@gmail.com>
2022-11-03 23:11:42 +0530

    purple theme

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'GetVuApp',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.deepPurple,
         ),
         home: const NavbarActivity());
   }
