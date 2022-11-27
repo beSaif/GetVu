87fe93e5698ada06b99e8659ee1781d740ac8486
jumainahkhan <jumainahkhan@gmail.com>
2022-11-27 11:55:56 +0530

    Messages Section added

--- lib/Screens/BookingScreen/components/Messages.dart
+++ lib/Screens/BookingScreen/components/Messages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:getvuapp/Screens/BookingScreen/components/ChatScreen.dart';
 
 class MessagesSection extends StatefulWidget {
   const MessagesSection({super.key});
@@ -8,12 +9,51 @@ class MessagesSection extends StatefulWidget {
 }
 
 class _MessagesSectionState extends State<MessagesSection> {
+  List<Map<String, dynamic>> chatdata = [
+    {
+      'name': 'John Smith',
+      'job': 'CEO at XYX Company',
+      'image': Image.asset('assets/Images/boygreen.png')
+    },
+    {
+      'name': 'Rita Lancaster',
+      'job': 'CEO at ABX Company',
+      'image': Image.asset('assets/Images/girlgreen.png')
+    },
+    {
+      'name': 'Piya Mitra',
+      'job': 'CEO at MND Company',
+      'image': Image.asset('assets/Images/girlblue.png')
+    },
+  ];
+  final Image image = Image.asset('assets/Images/My project.png');
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Text('Messages'),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              child: ListTile(
+                title: Text(
+                  chatdata[index]['name'],
+                ),
+                subtitle: Text(
+                  chatdata[index]['job'],
+                ),
+                leading: CircleAvatar(
+                  foregroundImage: chatdata[index]['image'].image,
+                  backgroundColor: Colors.blue[100],
+                  radius: 35,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => ChatScreen()));
+              },
+            );
+          },
+          itemCount: 3,
         ),
       ),
     );
